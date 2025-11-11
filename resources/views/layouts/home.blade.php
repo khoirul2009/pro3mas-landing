6c5af082db591af291fe7668d1c8ef61de4a2a81
khoirul afwan <khoirulafwan20@gmail.com>
2025-11-11 13:07:13 +0700

    feat: bootstrap

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -10,7 +10,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="icon" href="{{ asset('assets/icon.png') }}" type="image/png" />
     <link rel="stylesheet" href="{{ asset('css/main.css?v=') . filemtime(public_path('css/main.css')) }}" />
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     @stack('styles')
 </head>
 
@@ -84,6 +86,9 @@
     </div>
     <script src="https://unpkg.com/scrollreveal"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
     <script src="{{ asset('js/main.js') }}"></script>
     @stack('scripts')
 </body>
