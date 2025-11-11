02c2ac5c00c52acfd879e579c120755212228cf2
khoirul afwan <khoirulafwan20@gmail.com>
2025-11-11 13:10:12 +0700

    fix: a decoration none

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -14,6 +14,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     @stack('styles')
+    <style>
+        a {
+            text-decoration: none;
+        }
+    </style>
 </head>
 
 <body>
